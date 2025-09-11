ee443bc9c862c20a7292c14eba7941d980a5c8d8
nilesh834 <112253496+nilesh834@users.noreply.github.com>
2025-09-11 19:10:44 +0530

    Delete backend directory

--- backend/routes/bookingRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createBooking } from "../controller/bookingController.js";
-
-const router = express.Router();
-
-router.post("/create", createBooking);
-
-export default router;
